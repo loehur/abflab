54f368eb0f37b6c78513eb751c26c2a7aa817ef6
Luhur Gunawan <loehur@gmail.com>
2025-02-04 11:35:04 +0700

    gasss23ssssss2ssss

---
+++ app/Views/Load/Produk_Deskripsi/bingkai_medali.php
@@ -0,0 +1,6 @@
+<h7><b>BINGKAI MEDALI</b></h7>
+<br>ABADIKAN PRESTASIMU AGAR SELALU MENJADI PENYEMANGAT MERAIH JUARA!!!!!
+<br>
+<br>Spesifikasi:
+<br>Menggunakan MDF tebal 3mm dengan ukuran 74x44cm, Tersedia 28 Lubang, Tanpa Kaca.
+<br>Detail lubang medali ada pada gambar produk.
