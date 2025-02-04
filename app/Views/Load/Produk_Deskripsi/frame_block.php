fada4fa04c4425cfc0e9d305f4736e25704b5f7c
Luhur Gunawan <loehur@gmail.com>
2025-02-04 10:14:52 +0700

    gasss23ssssss2s

---
+++ app/Views/Load/Produk_Deskripsi/frame_block.php
@@ -0,0 +1,4 @@
+<h7><b>FRAME BLOCK:</b></h7>
+<br>Bingkai foto yang terbuat dari papan MDF (Medium Density Fiberboard)
+<br>Tampilannya modern dan minimalis, Foto dicetak menggunakan mesin digital lab berkualitas tinggi, Foto menempel secara permanen pada permukaan MDF, Foto bisa ditempel di dinding tanpa merusak.
+<br>Tersedia varian ukuran 20x20cm, 30x30cm, 50x50cm.
