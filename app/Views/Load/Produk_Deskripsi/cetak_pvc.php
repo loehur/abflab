1dbd2ebfe42ff5f0927b16bacc53963f10f0e186
Luhur Gunawan <loehur@gmail.com>
2025-02-06 13:58:52 +0700

    gas22

---
+++ app/Views/Load/Produk_Deskripsi/cetak_pvc.php
@@ -0,0 +1,21 @@
+<h7><b>CETAK PVC</b></h7>
+<br>Cetak kartu <b>BPJS, IDCARD, MYPERTAMINA, DLL</b>
+<br>Ukuran kartu PVC 85x55 MM, sama dengan ukuran kartu KTP, SIM, ATM.
+<br>Cetak kartu menggunakan bahan PVC adalah pilihan yang sangat cocok untuk memberikan identitas yang tahan lama dan profesional.
+<br>Bahan PVC, yang juga dikenal sebagai polivinil klorida, memberikan kelebihan ketahanan terhadap air, kelembaban, dan aus.
+<br>Proses cetak menggunakan teknologi mutakhir memberikan hasil cetak yang tajam, warna yang hidup, dan detail yang presisi.
+<br>
+<br>Kartu PVC memiliki ketebalan yang sesuai standar industri, memberikan kekuatan dan daya tahan ekstra. Permukaan kartu yang halus dan glossy menciptakan kesan yang elegan,
+<br>sementara tekstur PVC memberikan kekuatan struktural pada kartu. Bahan ini juga mudah untuk diberi lapisan laminasi,
+<br>menambah lapisan perlindungan dan menghasilkan tampilan yang lebih mantte.
+<br>
+<br>Proses cetak menggunakan teknologi cetak tinggi menghasilkan gambar dan teks yang jelas dan tajam, memastikan bahwa setiap detail pada kartu terlihat dengan sempurna.
+<br>Bahan PVC yang digunakan juga memungkinkan pencetakan gambar dengan kualitas foto, sehingga kartu tidak hanya berfungsi sebagai identifikasi,
+<br>tetapi juga memberikan kesan profesional dan estetis.
+<br>
+<br>Keunggulan lainnya adalah kemampuan kartu PVC untuk mendukung berbagai jenis cetakan, termasuk gambar, barcode, hologram, dan elemen keamanan lainnya.
+<br>Ini menjadikan kartu PVC ideal untuk keperluan identifikasi seperti kartu pengenal, kartu akses, kartu keanggotaan, dan kartu identitas lainnya.
+<br>
+<br>Dengan menggunakan bahan PVC untuk mencetak kartu, Anda dapat yakin bahwa kartu tersebut tidak hanya memenuhi kebutuhan fungsional,
+<br>tetapi juga memberikan tampilan yang profesional dan awet. Cetak kartu bahan PVC adalah solusi yang kokoh dan andal untuk kebutuhan identifikasi dan pengenalan
+<br>yang dapat diandalkan dalam berbagai situasi.
