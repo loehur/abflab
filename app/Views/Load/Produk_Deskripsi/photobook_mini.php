077da46e52fd9251b51e5d16ee0d70d1fd5359e2
Luhur Gunawan <loehur@gmail.com>
2025-03-28 09:31:16 +0700

    done code promo4

---
+++ app/Views/Load/Produk_Deskripsi/photobook_mini.php
@@ -0,0 +1,17 @@
+<b>Album Mini PhotoBook</b>
+
+<br>
+Album mungil yang praktis dan elegan, cocok untuk mengabadikan Momen wisuda, Rayakan pencapaian luar biasa Anda!Foto seru bersama teman, Simpan tawa dan kebersamaan selamanya!Kenangan perjalanan si kecil, Rekam tumbuh kembangnya dengan penuh cinta!
+Foto Liburan - Ciptakan momen perjalanan yang tidak terlupakan
+
+<br>
+<br>Deskripsi :
+<br>Ukuran 12X12cm
+<br>VARIAN SMALL TEBAL +-15MM
+<br>VARIAN LARGE TEBAL +-20MM
+
+<br>Siku Tumpul
+<br>Hard Paper
+<br>Hard Cover
+<br>Maksimal File 20 Foto
+<br>Sistem PO 4 Hari
