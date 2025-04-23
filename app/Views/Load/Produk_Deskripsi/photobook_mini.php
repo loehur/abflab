7c86d2c7d1a8db86801e2a38f5bc8bd4501c9d29
Luhur Gunawan <loehur@gmail.com>
2025-04-23 08:59:30 +0700

    mantap3

--- app/Views/Load/Produk_Deskripsi/photobook_mini.php
+++ app/Views/Load/Produk_Deskripsi/photobook_mini.php
@@ -1,7 +1,7 @@
 <b>Album Mini PhotoBook</b>
 
 <br>
-Album mungil yang praktis dan elegan, cocok untuk mengabadikan Momen wisuda, Rayakan pencapaian luar biasa Anda!Foto seru bersama teman, Simpan tawa dan kebersamaan selamanya!Kenangan perjalanan si kecil, Rekam tumbuh kembangnya dengan penuh cinta!
+Album mungil yang praktis dan elegan, cocok untuk mengabadikan Momen wisuda, Rayakan pencapaian luar biasa Anda! Foto seru bersama teman, Simpan tawa dan kebersamaan selamanya! Kenangan perjalanan si kecil, Rekam tumbuh kembangnya dengan penuh cinta!
 Foto Liburan - Ciptakan momen perjalanan yang tidak terlupakan
 
 <br>
