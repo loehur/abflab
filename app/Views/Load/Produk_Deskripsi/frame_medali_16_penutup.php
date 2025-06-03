a49e8aafbcfdfa52c349a66f2bcb1f6b6ce3e749
Luhur Gunawan <loehur@gmail.com>
2025-06-03 09:50:10 +0700

    warna444444434

---
+++ app/Views/Load/Produk_Deskripsi/frame_medali_16_penutup.php
@@ -0,0 +1,11 @@
+<pre>
+Spesifikasi :
+*Menggunakan MDF tebal 3mm ukuran 44x44cm
+*Jarak antar lobang kesamping 6.5cm, kebawah 9.5cm
+*Tersedia 16 Lobang Medali (Besar Lobang 0.5cm x 3.7cm)
+*Pakai Penutup menggunakan bahan Pet Clear
+*Terdapat Pilihan warna Wallpaper
+
+Barang Pre Order ya kakak :)
+Barang dikirim dari Kota Pekanbaru
+</pre>
